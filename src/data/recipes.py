ab4093eca73bb8386cb69e0c41948c68c6d2d9cc
polydome <polydome@outlook.com>
2019-12-09 12:25:54 +0100

    Add more recipes

--- src/data/recipes.py
+++ src/data/recipes.py
@@ -1,7 +1,13 @@
 from items import Recipe
 
 recipes = [
-    Recipe("młotek", 2, 3),
-    Recipe("nożyce", 8, 2),
-    Recipe("tarcza", 38, 20)
+    Recipe("małe ostrze", 2, 2),
+    Recipe("duże ostrze", 3, 10),
+    Recipe("młotek", 15, 6),
+    Recipe("nożyce", 26, 4),
+    Recipe("tarcza", 38, 20),
+    Recipe("grot", 20, 1),
+    Recipe("pręt", 1, 1),
+    Recipe("podkowa", 8, 5),
+    Recipe("kowadło", 40, 200)
 ]
